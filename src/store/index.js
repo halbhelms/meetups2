732c3761b344544f77f1812fbe0d051037d6efc8
Hal Helms <hal.helms@gmail.com>
2020-12-25 23:53:21 -0800

    added axios, users, and categories

--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,19 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    meetups: [],
+    categories: [
+      'Helping the Homeless',
+      'Preserving the Environment',
+      'Feeding the Hungry',
+      'Saving Animals'
+    ],
+    users: [
+      'Hosey Hoserson',
+      'Demo Sthenes',
+      'Johnny B. Good',
+      'Shallow Hal'
+    ]
   },
   mutations: {
   },
